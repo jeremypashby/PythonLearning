70644b7451563ffa70a4c9a1a368e7686aa9904b
jeremypashby <jeremypashby@JMA.local>
2016-06-30 21:12:33 +0100

    some changes

--- Challenge5/Challenge5.py
+++ Challenge5/Challenge5.py
@@ -1,16 +1,21 @@
+def translate(transword):
+
+    consonant = "bcdfghjklmnpqrstvwxyz"
+    finishword = ""
+    for char in transword:
+        if char in consonant:
+            finishword += char + 'o' + char
+        else:
+            finishword += char
+    return finishword
 
 
+def main():
+    TRANSWORD = "this is fun"
+    TRANSWORD2 = "this is not fun"
+    print translate(transword=TRANSWORD)
+    print translate(transword=TRANSWORD2)
 
-#print consonant
-def translate():
 
-	#while True:
-	StartWord = ("tt")
-	consonant = ("bcdfghjklmnpqrstvwxyz")
-    	for char in StartWord:
-        	if char in consonant:
-        		FinishWord = StartWord.replace(char, char + 'o' + char)
-    			print FinishWord
-	
 if __name__ == "__main__":
-	translate()
+    main()
