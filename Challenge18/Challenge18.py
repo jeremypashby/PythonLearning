7c4404925608f85374e0a16db3e6aa2d6affc239
jeremypashby <jeremypashby@gmail.com>
2016-07-06 10:32:56 +0100

    challenge18 done

--- Challenge18/Challenge18.py
+++ Challenge18/Challenge18.py
@@ -1,20 +1,28 @@
-word = "The quick brown fox jumps over the lazy dog"
-nospaceword = word.replace(" ", "")
-alpha = "abcdefghijklmnopqrstuvwxyz"
-alphanumber = 26
-check = 0
-#print nospaceword
-if nospaceword < alphanumber:
-    print "nope!"
-else:
-    #print "maybe!"
-    for i in alpha:
-        if i in nospaceword:
-            check = check+1
-            #print check
-    if check == 26:
-        print check
-        print "true"
+def is_panagram(Sentance):
+
+    nospaceword = Sentance.replace(" ", "")
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    alphanumber = 26
+    check = 0
+
+    if nospaceword < alphanumber:
+        print "nope!"
     else:
-        print  check
-        print "false"
+        for i in alpha:
+            if i in nospaceword:
+                check = check+1
+        if check == 26:
+            print check
+            print "true"
+        else:
+            print  check
+            print "false"
+
+
+def main():
+    word = "The quick brown fox jumps over the lazy dog"
+    is_panagram(word)
+
+
+if __name__ == "__main__":
+    main()
