81c50bc8478fb89a6fdd878bb92ab9d2796c05ba
jeremypashby <jeremypashby@gmail.com>
2016-07-13 10:41:18 +0100

    updates

--- DailyChallenge/27062016(273)/challenge.py
+++ DailyChallenge/27062016(273)/challenge.py
@@ -1,17 +1,26 @@
-#def main():
+def convert(value):
+	import math
+
+	number = float(value[:-2])
+	firstarg = value[-2]
+	secondarg = value[-1]
+
+
+	if firstarg == "r":
+		x = math.degrees(number)
+		x = round(x)
+		return "{0:g}d".format(x) 
+	if firstarg == "d":
+		y = math.radians(number)
+		y = round(y,2)
+		return "{0:g}r".format(y) 
+
+
+def main():
+	value = raw_input("enter in value: ")
+	print convert(value)
 
 
 
-import math
-
-x = math.degrees(3.1416)
-x = round(x,2)
-print "{0:g} degrees".format(x)  
-
-
-y = math.radians(90)
-y = round(y,2)
-print "{0:g} radians".format(y) 
-
-#print main()
-
+if __name__ == "__main__":
+	main()
