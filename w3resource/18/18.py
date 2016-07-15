edc88f7cab6d73fc9e75e126b00bb239fb6da132
jeremypashby <jeremypashby@gmail.com>
2016-07-15 14:19:23 +0100

    more done

---
+++ w3resource/18/18.py
@@ -0,0 +1,13 @@
+def value(f_num, s_num, t_num):
+
+	x = f_num + s_num + t_num
+
+
+	if f_num == s_num == t_num:
+		x = x*3
+		return x
+	else:
+		return x
+
+
+print value(10,10,11)
---
+++ w3resource/20/20.py
@@ -0,0 +1,9 @@
+def string_values(word,number):
+	words = ""
+	for i in range(number):
+		
+		words += word
+	return words
+
+
+print string_values('hello', 3)
